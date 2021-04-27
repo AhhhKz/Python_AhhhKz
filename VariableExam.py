72ea9d24045b3a41b2bc558522857dcbdf79ab4b
AhhhKz <arkkunatgan@gmail.com>
2021-04-27 18:09:18 +0700

    Add File Python

---
+++ VariableExam.py
@@ -0,0 +1,7 @@
+name = "Kunatgan Rattanagool"
+age = 20
+height = 176
+weight = 67
+print("My name is",name,",I'm ",age,"Year old.")
+print("Height :",height)
+print("Weight :",weight)
---
+++ lecture16.py
@@ -0,0 +1,6 @@
+print(type("Hello World"))
+print(type(10))
+print(type(10.1))
+print(type(True))
+print(type(1j))
+print(type(10+3.5))
