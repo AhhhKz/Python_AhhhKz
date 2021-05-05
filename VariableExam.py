04e0f9d70c86a534f49a2220288a2f9e8633ac71
AhhhKz <arkkunatgan@gmail.com>
2021-05-05 15:21:58 +0700

    Edit Variable

--- VariableExam.py
+++ VariableExam.py
@@ -2,6 +2,7 @@ name = "Kunatgan Rattanagool"
 age = 20
 height = 176
 weight = 67
+print("Sawasdeekub")
 print("My name is",name,",I'm ",age,"Year old.")
 print("Height :",height)
 print("Weight :",weight)
